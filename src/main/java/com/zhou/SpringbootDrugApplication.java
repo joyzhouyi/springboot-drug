37208f123278dbd686c0ab2aab9cd02903e614df
zhouYi <2643983350@qq.com>
2021-03-24 20:42:25 +0800

    first commit

---
+++ src/main/java/com/zhou/SpringbootDrugApplication.java
@@ -0,0 +1,13 @@
+package com.zhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDrugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDrugApplication.class, args);
+    }
+
+}
